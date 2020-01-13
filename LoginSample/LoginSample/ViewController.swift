5159b366cea25ff46f5a2b817bdcaea721ac828e
Rusen Topcu <rusentopcu@gmail.com>
2020-01-13 11:50:27 +0300

    Proje Oluşturuldu

---
+++ LoginSample/LoginSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginSample
+//
+//  Created by Rusen Topcu on 13.01.2020.
+//  Copyright © 2020 Rusen Topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
